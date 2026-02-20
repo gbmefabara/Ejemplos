1fda3e827d68183f1c183bfb38a93390e42cbb68
gbmefabara <efabara@gbmpaulovi.edu.ec>
2026-02-20 09:35:36 -0500

    Ejemplos
    Dar espacios

--- leds.py
+++ leds.py
@@ -1,17 +1,21 @@
 from machine import Pin
 import time
+
 led = Pin(12, Pin.OUT)
 led.value(1)
 time.sleep(1)
 led.off()
+
 led = Pin(18, Pin.OUT)
 led.value(2)
 time.sleep(1)
 led.off()
+
 led = Pin(19, Pin.OUT)
 led.value(3)
 time.sleep(1)
 led.off()
+
 led = Pin(13, Pin.OUT)
 led.value(4)
 time.sleep(1)
